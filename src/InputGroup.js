168463e62c3100b7aa3c5ba12774164144aaf464
Dan Ott <dan@dtott.com>
2020-02-10 13:39:22 -0500

    Pass props through

--- src/InputGroup.js
+++ src/InputGroup.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Alert from './Alert'
 
-const InputGroup = ({ children, error }) => (
-  <div>
+const InputGroup = ({ children, error, ...props }) => (
+  <div {...props}>
     {children}
     {error && <Alert>{error}</Alert>}
   </div>
