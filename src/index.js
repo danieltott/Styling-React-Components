b472aab4ce247e47759e6cd8b3a7b62e4c599f16
Dan Ott <dan@dtott.com>
2020-02-10 10:24:24 -0500

    Remove styles for no-styles example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
