28185837228f19c6bb75c998748a36014e14bb29
Dan Ott <dan@dtott.com>
2020-02-11 08:30:52 -0500

    Add dark/light theme switcher

--- src/StyledWrapper.js
+++ src/StyledWrapper.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { ThemeProvider, createGlobalStyle } from 'styled-components'
-import theme from './theme'
+import React, { useState } from 'react'
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components'
+import { light, dark } from './theme'
 
 const GlobalStyle = createGlobalStyle`
 html {
@@ -11,14 +11,32 @@ html {
 body {
   font-family: ${props => props.theme.font.family};
   padding: 1em;
+  background: ${props => props.theme.colors.bodyBg};
+  color: ${props => props.theme.colors.bodyCopy};
 }
 `
 
-const StyledWrapper = ({ children }) => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    {children}
-  </ThemeProvider>
-)
+const ThemeToggler = styled.button`
+  position: fixed;
+  top: 5px;
+  right: 5px;
+`
+
+const StyledWrapper = ({ children }) => {
+  const [isDark, setIsDark] = useState(false)
+  return (
+    <ThemeProvider theme={isDark ? dark : light}>
+      <GlobalStyle />
+      <ThemeToggler
+        onClick={() => {
+          setIsDark(!isDark)
+        }}
+      >
+        {isDark ? '☀️' : '🌚'}
+      </ThemeToggler>
+      {children}
+    </ThemeProvider>
+  )
+}
 
 export default StyledWrapper
--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,4 @@
-const theme = {
+export const light = {
   font: {
     family: `-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
     'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
@@ -8,6 +8,8 @@ const theme = {
   colors: {
     danger: 'red',
     success: 'green',
+    bodyBg: '#fff',
+    bodyCopy: '#333',
   },
   breakpoints: {
     phone: '480px',
@@ -16,4 +18,23 @@ const theme = {
   },
 }
 
-export default theme
+export const dark = {
+  background: '#fff',
+  font: {
+    family: `-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
+    'Segoe UI Symbol'`,
+  },
+  borderRadius: '5px',
+  colors: {
+    danger: 'darkred',
+    success: 'green',
+    bodyBg: '#333',
+    bodyCopy: '#fff',
+  },
+  breakpoints: {
+    phone: '480px',
+    tablet: '768px',
+    desktop: '1024px',
+  },
+}
